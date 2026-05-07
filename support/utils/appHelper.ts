4ad31252f35ab8fcf5179ec54e64e594bfecbcf7
Eduardo Stegani <estegani@vidalink.com.br>
2026-05-07 18:39:12 -0300

    ajustes no reset e login para uso em befores e chamada na spec

--- support/utils/appHelper.ts
+++ support/utils/appHelper.ts
@@ -2,15 +2,18 @@ import { loginScreen, homeScreen, profileScreen } from '../../screens/index.js'
 import { only } from 'node:test';
 
 export class AppHelper {
-    static async resetAndLogin(cpf: string, password: string ) {
+    static async resetApp() {
         const appId = driver.isAndroid 
             ? 'com.astl.vidalink.beta' 
             : 'br.com.vidalink.beta';
 
         try { await driver.terminateApp(appId);  } catch (e) { }
         await driver.activateApp(appId);
+    }
 
+    static async login(cpf: string, password: string ) {
+        await this.resetApp();
         await loginScreen.login(cpf, password);
         await homeScreen.checkDashboard();
-    }
+    }    
 }
