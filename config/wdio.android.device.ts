fe28243c9093ebd97f0c5addcf250689a1a8ba76
Eduardo Stegani <estegani@vidalink.com.br>
2026-05-07 18:35:34 -0300

    rename para wdio configs scripts package e add config device

---
+++ config/wdio.android.device.ts
@@ -0,0 +1,50 @@
+import path from 'node:path'
+import type { Options, Frameworks } from '@wdio/types'
+import { globalBeforeEach, globalAfterEach } from '../support/hooks/global.hooks.js'
+
+export const config = {
+  runner: 'local',
+  port: 4723,
+  path: '/',
+  specs: ['../tests/specs/**/*.ts'],
+  framework: 'mocha',
+  logLevel: 'error',
+  maxInstances: 1,
+  waitforTimeout: 30000,
+  waitforInterval: 1000,
+  services: ['appium'],
+  reporters: [
+    'spec',
+    ['allure', {
+      outputDir: 'allure-results'
+    }]
+  ],
+  mochaOpts: { timeout: 600000 },
+
+  before: async function () {
+    await globalBeforeEach()
+  },
+
+  beforeTest: function (test, context) {
+    console.log(`▶️  Runing Scenario: ${test.title}`);
+  },
+
+  afterTest: globalAfterEach,
+
+  capabilities:
+    [{
+      platformName: 'Android',
+      'appium:automationName': 'UiAutomator2',
+      'appium:deviceName': 'Galaxy_S25',
+      'appium:udid': 'RQGL209A1AA',
+      'appium:app': path.join(process.cwd(), 'app', 'Vidalink-betaDebug-5_10_1.apk'),
+      'appium:appPackage': 'com.astl.vidalink.beta',
+      'appium:appWaitActivity': '*',
+      'appium:appWaitDuration': 30000,
+      'appium:appWaitPackage': 'com.astl.vidalink.beta',
+      'appium:newCommandTimeout': 240,
+      'appium:autoGrantPermissions': true,
+      'appium:noReset': true,
+      'appium:fullReset': false,
+    }]
+} satisfies Options.Testrunner & { capabilities: WebdriverIO.Capabilities[] }
