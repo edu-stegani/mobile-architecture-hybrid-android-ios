97a8338e189bd77402a8eebe2db7fb74e194b210
Eduardo Stegani <estegani@vidalink.com.br>
2026-06-08 15:25:42 -0300

    implementacao card ios

--- screens/ios/card.screen.ts
+++ screens/ios/card.screen.ts
@@ -5,46 +5,46 @@ class CardIOS extends BaseScreen {
 
     // ====== SELECTORS ======
     get cardTab() {
-        return '//android.widget.TextView[@text="Cartão"]'
+        return '//XCUIElementTypeButton[@name="Cartão"]'
     }
 
     get card(){
-        return '//android.view.ViewGroup[@resource-id="com.astl.vidalink.beta:id/clBackground"]'
+        return '//XCUIElementTypeCell'
     }
 
     get cardNumber(){
-        return '//android.widget.TextView[@resource-id="com.astl.vidalink.beta:id/tvCardNumber"]'
+        return '//XCUIElementTypeStaticText[@name="lblNumber"]'
     }
 
     get btnCopyNumberCard(){
-        return '//android.widget.Button[@resource-id="com.astl.vidalink.beta:id/btnCopyNumber"]'
+        return '(//XCUIElementTypeButton[@name="Copiar número do cartão"])[1]'
     }
     // ======== ACTIONS ========
     async viewCardAndName(name: string) {
-        const cardName = `${this.card}//android.widget.TextView[contains(@text, "${name}")]`
+        const cardName = `${this.card}//*[contains(@name, "${name}")]`
         await expect($(cardName)).toBeDisplayed()
 
         const cardNumber = `${cardName}/..${this.cardNumber}`
-        const number = await $(cardNumber).getText()
+        const number = await $(cardNumber).getValue()
         return number
     }
 
     // ======== METHODS ========
     async navigationAndViewCards(name1: string, name2: string, name3: string) {
         const cardNumber1 = await this.viewCardAndName(name1)
-        await this.SwipeLeftCoordinates(300)
+        await this.SwipeLeftCoordinates(220)
         const cardNumber2 = await this.viewCardAndName(name2)
-        await this.SwipeLeftCoordinates(300)
+        await this.SwipeLeftCoordinates(220)
         const cardNumber3 = await this.viewCardAndName(name3)
 
-        await this.SwipeRightCoordinates(300)
-        await this.SwipeRightCoordinates(300)
+        await this.SwipeRightCoordinates(220)
+        await this.SwipeRightCoordinates(220)
 
         await this.waitAndClick($(this.cardTab))
 
-        const numberCardScreen  = `${this.card}//android.widget.TextView[contains(@text, "${cardNumber1}")]`
-        const numberCardScreen2 = `${this.card}//android.widget.TextView[contains(@text, "${cardNumber2}")]`
-        const numberCardScreen3 = `${this.card}//android.widget.TextView[contains(@text, "${cardNumber3}")]`
+        const numberCardScreen  = `${this.card}//*[contains(@value, "${cardNumber1}")]`
+        const numberCardScreen2 = `${this.card}//*[contains(@value, "${cardNumber2}")]`
+        const numberCardScreen3 = `${this.card}//*[contains(@value, "${cardNumber3}")]`
         
         await expect($(numberCardScreen)).toBeDisplayed()
         await expect($(this.btnCopyNumberCard)).toBeEnabled()
