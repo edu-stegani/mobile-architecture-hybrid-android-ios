c192610f29756b3b06335ead8b131358cae8381c
Eduardo Stegani <estegani@vidalink.com.br>
2026-06-11 14:05:13 -0300

    implementacao extrato ios

--- screens/ios/extrato.screen.ts
+++ screens/ios/extrato.screen.ts
@@ -4,69 +4,66 @@ import BaseScreen from '../shared/base.screen.js'
 class ExtratoIOS extends BaseScreen {
     // ====== SELECTORS ======
     get tittleSaved() {
-        return $('')
+        return $('//XCUIElementTypeStaticText[contains(@name, "Economizei: R$")]')
     }
 
     get lastMonths() {
-        return $('')
-    }
-
-    get valueSaved() {
-        return $('')
+        return $('//XCUIElementTypeStaticText[contains(@name, "nos últimos 12 meses")]')
     }
 
     get cardReport() {
-        return ''
+        return '//XCUIElementTypeCell/XCUIElementTypeOther/XCUIElementTypeOther'
     }
 
     get btnSeePurchases() {
-        return $('')
+        return $('//XCUIElementTypeButton[contains(@name, "compras")]')
     }
 
-    get dashboardSubsidyBalance() {
-        return $('')
+    get dashboardValueDisponible() {
+        return '//XCUIElementTypeStaticText[contains(@name, "Disponível R$")]'
+    }
+
+    get dashboardSubsidy(){
+        return $('//XCUIElementTypeStaticText[@name="Saldo do Subsídio"]')
     }
 
     get selectUserFamily() {
-        return $('')
-    }
-
-    get mesesExtrato() {
-        return $('')
+        return $('//XCUIElementTypeButton[contains(@name, "Compras")]')
     }
 
     get abaMonth(){
-        return ``
+        return `//XCUIElementTypeCollectionView/XCUIElementTypeCell`
     }
 
     get monthsSummary(){
-        return $('')
+        return $('~Minhas despesas:')
     }
 
     // ======== ACTIONS ========
     async viewScreenExtrato(name: string) {
         const tituloEconomizei = this.tittleSaved
         const ultimosMeses = this.lastMonths
-        const valorEconomizado = this.valueSaved
         const cardReport = this.cardReport
-        const cardReportAndName = `${cardReport}//*[contains(@text, "${name}")]`
-        const dashboardSubsidyBalance = this.dashboardSubsidyBalance
+        const cardReportAndName = `${cardReport}//*[contains(@name, "${name}")]`
+        const dashboardSubsidy = this.dashboardSubsidy
+        const dashboardSaldo = `(${this.dashboardValueDisponible})[2]`
 
         await tituloEconomizei.waitForDisplayed({ interval: 1000 })
         await ultimosMeses.waitForDisplayed({ interval: 1000 })
-        await expect(valorEconomizado).toBeDisplayed({ interval: 1000 })
         await expect($(cardReport)).toBeDisplayed({ interval: 1000 })
-        await expect($(cardReportAndName)).toBeDisplayed({ interval: 1000 })
 
         try {
-            await dashboardSubsidyBalance.waitForDisplayed({ timeout: 10000, interval: 1000 })
+            await dashboardSubsidy.waitForDisplayed({ timeout: 10000, interval: 1000 })
+            await $(dashboardSaldo).waitForDisplayed({ timeout: 10000, interval: 1000 })
         } catch (e) { console.log('Dashboard saldo subsídio não visível, massa CT Pirelli.') }
+        
+        await this.SwipeLeftCoordinates(450)
+        await expect($(cardReportAndName)).toBeDisplayed({ interval: 1000 })
     }
 
     async seePurchases() {
         const btnVerCompras = this.btnSeePurchases
         const selectUserFamily = this.selectUserFamily
-        const mesesExtrato = this.mesesExtrato
         const mes = this.abaMonth
         const mes1 = `(${mes})[1]`
         const mes2 = `(${mes})[2]`
@@ -75,7 +72,6 @@ class ExtratoIOS extends BaseScreen {
         await this.waitAndClick(btnVerCompras)
 
         await selectUserFamily.waitForDisplayed({ interval: 1000 })
-        await expect(mesesExtrato).toBeDisplayed({ interval: 1000 })
 
         await this.waitAndClick($(mes2))
         await this.waitAndClick($(mes1))
--- tests/specs/extrato.spec.ts
+++ tests/specs/extrato.spec.ts
@@ -4,7 +4,7 @@ import postgresHelper from '../../support/utils/postgresHelper.js'
 import oracleHelpers from '../../support/utils/oracleHelpers.js'
 import { AppHelper } from '../../support/utils/appHelper.js'
 
-const user = data.users.Isis
+const user = data.users.Marcia
 const userPirelli = data.users.Edson
 
 describe('Validação de tela em Extrato', () => {
