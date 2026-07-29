e90d76e3d2230299e172d91e4b0726ac53e21a44
Eduardo Stegani <estegani@vidalink.com.br>
2026-07-29 19:00:11 -0300

    ajustes nos ganchos de execução via device e browserstack e ajustes no evio de receita via api

--- support/hooks/global.hooks.ts
+++ support/hooks/global.hooks.ts
@@ -2,6 +2,7 @@ import type { Options } from '@wdio/types'
 import { Frameworks } from '@wdio/types';
 import fs from 'fs';
 import path from 'path';
+import allure from '@wdio/allure-reporter'
 
 export const globalBeforeEach = async () => {
   const platform = driver.isAndroid ? 'Android' : 'iOS';
@@ -11,41 +12,39 @@ export const globalBeforeEach = async () => {
 export const globalAfterEach: Options.Testrunner['afterTest'] = async (
   test,
   context,
-  { passed, error }
+  { error, passed }
 ) => {
+  const message = passed
+    ? `✅ Sucesso: ${test.title}`
+    : `❌ Falha: ${test.title}`;
+
+  console.log(message);
+
+  if (error) {
+    console.log(`Motivo: ${error.message}`);
+  }
+
+  // Tira screenshot em ambos os casos (sucesso e falha)
+  const screenshot = await browser.takeScreenshot();
+  allure.addAttachment(`Screenshot - ${passed ? 'SUCESSO' : 'FALHA'}`, Buffer.from(screenshot, 'base64'), 'image/png');
+
+  // Salva o screenshot em um arquivo apenas em caso de falha para fácil acesso
   if (!passed) {
-    // 1. Define o diretório baseado na plataforma
     const platform = driver.isAndroid ? 'android' : 'ios';
     const dir = path.resolve(process.cwd(), `screenshots/${platform}`);
-
-    // 2. Garante que o diretório existe 
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir, { recursive: true });
-    }
-
-    // 3. Limpa o título do teste para virar nome de arquivo (remove espaços e símbolos)
+    fs.mkdirSync(dir, { recursive: true });
     const cleanTitle = test.title.replace(/[^a-z0-9]/gi, '_').toLowerCase();
     const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
     const fileName = path.join(dir, `FAIL_${cleanTitle}_${timestamp}.png`);
-
-    // 4. Salva o Screenshot
     await browser.saveScreenshot(fileName);
-
-    console.error(`❌ Teste falhou [${platform.toUpperCase()}]: ${test.title}`);
     console.log(`Screenshot salvo em: ${fileName}`);
-
-    if (error) {
-      console.log(`Motivo: ${error.message}`);
-    }
   }
 };
 
-export const setBSName = async (specs: string[]) => {
-  const specName = path.basename(specs[0]);
-
+export const setBSSessionName = async (testTitle: string) => {
   await browser.execute(`browserstack_executor: ${JSON.stringify({
     action: 'setSessionName',
-    arguments: { name: specName }
+    arguments: { name: testTitle }
   })}`);
 };
 
